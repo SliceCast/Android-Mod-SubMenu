eeb6908c6277ea25ec15bdefc4f8ddab648baf9d
Tou <77551770+SliceCast@users.noreply.github.com>
2021-08-02 21:36:29 -0400

    Update Main.cpp

--- app/src/main/jni/Main.cpp
+++ app/src/main/jni/Main.cpp
@@ -1,67 +1,106 @@
 #include <list>
 #include <vector>
 #include <string.h>
-#include <GLES2/gl2.h>
 #include <pthread.h>
 #include <cstring>
 #include <jni.h>
 #include <unistd.h>
 #include <fstream>
+#include <iostream>
+//#include <And64InlineHook/And64InlineHook.hpp>
+#include "Includes/Vector2.h"
+#include "Includes/Rect.h"
+#include "Includes/Color.h"
+#include "Includes/Quaternion.h"
+#include "Includes/Logger.h"
 #include "Includes/obfuscate.h"
-#include "KittyMemory/MemoryPatch.h"
-#include "Includes/Logger.h"
 #include "Includes/Utils.h"
+#include "KittyMemory/MemoryPatch.h"
 #include "Menu.h"
-#include "Toast.h"
-#include <And64InlineHook/And64InlineHook.hpp>
-#include <chrono>
-#include "IL2CppResolver/SigUtils.h"
-#include "IL2CppResolver/Resolver.h"
-#include <pthread.h>
-#include <jni.h>
-#include <GLES2/gl2.h>
-#include <dlfcn.h>
-#include "KittyMemory/MemoryPatch.h"
-#include "Includes/Logger.h"
-#include "Unity/Unity.h"
-#include "Includes/Utils.h"
-#include <pthread.h>
-#include <jni.h>
-#include "KittyMemory/MemoryPatch.h"
-#include "Includes/Logger.h"
-#include <GLES2/gl2.h>
-#include <dlfcn.h>
-#include "KittyMemory/MemoryPatch.h"
-#include "Includes/Logger.h"
-#include "Includes/Utils.h"
-#include "GameData/GameData.h"
-#include "GameData/Offsets.h"
-#include "Unity/Vector2.h"
-#include "Unity/Rect.h"
-#include "Unity/Color.h"
-#include "Unity/Quaternion.h"
-#include <sstream>
-#include <codecvt>
-
 
 #if defined(__aarch64__)
-//#include <And64InlineHook/And64InlineHook.hpp>
+
+#include <And64InlineHook/And64InlineHook.hpp>
+
+#define HOOK(offset, ptr, orig) A64HookFunction((void *)getAbsoluteAddress(targetLibIL2CPP, string2Offset(OBFUSCATE_KEY(offset, 23479432523588))), (void *)ptr, (void **)&orig)
+
 #else
 
-#include <Substrate/SubstrateHook.h>
 #include <Substrate/CydiaSubstrate.h>
+
+#define HOOK(offset, ptr, orig) MSHookFunction((void *)getAbsoluteAddress(targetLibIL2CPP, string2Offset(OBFUSCATE_KEY(offset, 23479432523588))), (void *)ptr, (void **)&orig)
 
 #endif
 
+void hooking(void *orig_fcn, void* new_fcn, void **orig_fcn_ptr)
+{
+#if defined(__aarch64__)
+    A64HookFunction(orig_fcn, new_fcn, orig_fcn_ptr);
+#else
+    MSHookFunction(orig_fcn, new_fcn, orig_fcn_ptr);
+#endif
+}
 
-// fancy struct for patches for kittyMemory
+//!!! KittyMemory || Hex-Patching !!!//
 struct My_Patches {
-     MemoryPatch procodeleecher,bouzeleecher,voidclown;
+    // let's assume we have patches for these functions for whatever game
+    // like show in miniMap boolean function
+    MemoryPatch GodMode, GodMode2, SliderExample;
+    // etc...
 } hexPatches;
 
+bool isTest, isTest2;
 
+//!!! Hooks !!!//
 
-#define targetLibName OBFUSCATE("libil2cpp.so")
+bool (*_method)(void *instance);
+bool method(void *instance) {
+    if (instance != NULL){
+        if(isTest) {
+            return true;
+        }
+    }
+    return _method(instance);
+}
+
+int (*_method2)(void *instance);
+int method2(void *instance){
+    if (instance != NULL) {
+        if(isTest2) {
+            return 99;
+        }
+    }
+    return _method2(instance);
+}
+
+void *hack_thread(void *) {
+    LOGI(OBFUSCATE("pthread created"));
+
+    do {
+        sleep(1);
+    } while (!isLibraryLoaded(targetLibIL2CPP));
+
+    LOGI(OBFUSCATE("%s has been loaded"), (const char *) targetLibIL2CPP);
+
+    //!!! A64HookFunction AKA ARM64, (DOES NOT WORK FOR SOME REASON!) !!!//
+
+    //!!! This template supports ARMV-7 for now, if you know the solution, contribute or issue a page for ARM64 !!!//
+
+    hexPatches.GodMode = MemoryPatch::createWithHex(targetLibIL2CPP, //Normal obfuscate
+                                                    string2Offset(OBFUSCATE("0x000000")),
+                                                    OBFUSCATE("00 00 A0 E3 1E FF 2F E1"));
+    //You can also specify target lib like this
+    hexPatches.GodMode2 = MemoryPatch::createWithHex("libil2cpp.so",
+                                                     string2Offset(OBFUSCATE_KEY("0x000000", 23479432523588)), //64-bit key in decimal
+                                                     OBFUSCATE_KEY("01 00 A0 E3 1E FF 2F E1", 0x3FE63DF21A3B)); //64-bit key in hex works too
+
+    hooking((void*)getAbsoluteAddress(targetLibIL2CPP, string2Offset(OBFUSCATE_KEY("0x000000", 23479432523588))), (void *) method, (void **)&_method);
+
+    HOOK("0x000000", method2, _method2);
+
+    LOGI(OBFUSCATE("Done"));
+    return NULL;
+}
 
 extern "C" {
 JNIEXPORT jobjectArray
@@ -100,8 +139,8 @@ Java_com_simplefucker_source_ModMenu_getPlayer(JNIEnv *env, jobject activityObje
 
     const char *features[] = {
             OBFUSCATE("0_Category_Player Category"),
-            OBFUSCATE("0_ButtonOnOff_Click Me"),
-            OBFUSCATE("0_SeekBar_Preview SeekBar_1_13131"),
+            OBFUSCATE("100_Toggle_God Mode"),
+            OBFUSCATE("151_SeekBar_Preview SeekBar_1_13131"),
     };
 
     int Total_Feature = (sizeof features /
@@ -124,9 +163,9 @@ Java_com_simplefucker_source_ModMenu_getVisuals(JNIEnv *env, jobject activityObj
     jobjectArray ret;
 
     const char *features[] = {
-            OBFUSCATE("0_Category_Esp"),
-            OBFUSCATE("0_ButtonOnOff_Click Me"),
-            OBFUSCATE("0_SeekBar_Preview SeekBar_1_13131"),
+            OBFUSCATE("0_Category_Weapon"),
+            OBFUSCATE("101_Toggle_Ammo"),
+            OBFUSCATE("151_SeekBar_Preview SeekBar_1_13131"),
     };
 
     int Total_Feature = (sizeof features /
@@ -143,56 +182,7 @@ Java_com_simplefucker_source_ModMenu_getVisuals(JNIEnv *env, jobject activityObj
 
     return (ret);
 }
-JNIEXPORT jobjectArray
-JNICALL
-Java_com_simplefucker_source_ModMenu_getWeapon(JNIEnv *env, jobject activityObject) {
-    jobjectArray ret;
 
-    const char *features[] = {
-            OBFUSCATE("0_Category_Your Category"),
-            OBFUSCATE("0_ButtonOnOff_Click Me"),
-            OBFUSCATE("0_SeekBar_Preview SeekBar_1_13131"),
-    };
-
-    int Total_Feature = (sizeof features /
-                         sizeof features[0]); //Now you dont have to manually update the number everytime;
-    ret = (jobjectArray)
-            env->NewObjectArray(Total_Feature, env->FindClass(OBFUSCATE("java/lang/String")),
-                                env->NewStringUTF(""));
-    int i;
-    for (i = 0; i < Total_Feature; i++)
-        env->SetObjectArrayElement(ret, i, env->NewStringUTF(features[i]));
-
-    pthread_t ptid;
-    pthread_create(&ptid, NULL, antiLeech, NULL);
-
-    return (ret);
-}
-JNIEXPORT jobjectArray
-JNICALL
-Java_com_simplefucker_source_ModMenu_getMisc(JNIEnv *env, jobject activityObject) {
-    jobjectArray ret;
-
-    const char *features[] = {
-            OBFUSCATE("0_Category_The Preview:D"),
-            OBFUSCATE("0_SeekBar_Preview SeekBar_1_4"),
-            OBFUSCATE("0_ButtonOnOff_Click Me"),
-    };
-
-    int Total_Feature = (sizeof features /
-                         sizeof features[0]); //Now you dont have to manually update the number everytime;
-    ret = (jobjectArray)
-            env->NewObjectArray(Total_Feature, env->FindClass(OBFUSCATE("java/lang/String")),
-                                env->NewStringUTF(""));
-    int i;
-    for (i = 0; i < Total_Feature; i++)
-        env->SetObjectArrayElement(ret, i, env->NewStringUTF(features[i]));
-
-    pthread_t ptid;
-    pthread_create(&ptid, NULL, antiLeech, NULL);
-
-    return (ret);
-}
 JNIEXPORT void JNICALL
 Java_com_simplefucker_source_SavedPrefs_Changes(JNIEnv *env, jclass clazz, jobject obj,
                                                 jint feature, jint value, jboolean boolean, jstring str) {
@@ -205,26 +195,21 @@ Java_com_simplefucker_source_SavedPrefs_Changes(JNIEnv *env, jclass clazz, jobje
     //!!! BE CAREFUL NOT TO ACCIDENTLY REMOVE break; !!!//
 
     switch (feature) {
-        case 1:
-        break;
+        case 100:
+            isTest = boolean;
+            break;
+        case 101:
+            isTest2 = boolean;
+            break;
     }
 }
 }
 
-void *IoioioO(void *) {
 
-#if defined(__aarch64__)
-
-#else
-
-#endif
-
-    return NULL;
-}
 
 __attribute__((constructor))
 void lib_main() {
     pthread_t ptid;
-    pthread_create(&ptid, NULL, IoioioO, NULL);
+    pthread_create(&ptid, NULL, hack_thread, NULL);
 }
 
